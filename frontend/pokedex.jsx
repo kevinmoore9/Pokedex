391d7bf71c9b68690e3e654973cb55a456382156
Kevin Moore <kmoore5194@gmail.com>
2017-01-04 14:08:42 -0800

    add selector

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions.js';
 import { fetchAllPokemon } from './util/api_util.js';
 import { configureStore } from './store/store';
+import { selectAllPokemon } from './reducers/selectors';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
@@ -11,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
   window.receiveAllPokemon = receiveAllPokemon;
   window.fetchAllPokemon = fetchAllPokemon;
   window.requestAllPokemon = requestAllPokemon;
+  window.selectAllPokemon = selectAllPokemon; 
   window.store = store;
 
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
